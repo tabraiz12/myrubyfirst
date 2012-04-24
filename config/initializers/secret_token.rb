61e8957da6be35389bc14fbe4cc118ee0103e8fc
tabraiz <tabraiz12@gmail.com>
2012-04-24 10:18:21 +0530

    myfirstruby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testproject::Application.config.secret_token = '8f6be45bcfc4ac8d3e03f9e893e37a50a9706f8a10ccd64889d3d9225456025c50036f1c56cc0cee8c19144494d261b16db78c7a8f17ca8a7960ba21a65cde17'
